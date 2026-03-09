e38289ff599cf19065402d094d11dde8ee5a1a03
Prajwal <pvashisth2000@gmail.com>
2026-03-10 01:00:27 +0530

    api-url-add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+   baseURL: "https://voice-ai-optimizer-api.onrender.com/api"
 });
 
 export const runCopilot = (prompt) =>
